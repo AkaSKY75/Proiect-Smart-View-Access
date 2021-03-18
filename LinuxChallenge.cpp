fcb8706863656465080df6809b687dfda67e9c0f
MirceaAurelian <63643923+MirceaAurelian@users.noreply.github.com>
2021-03-18 14:54:38 +0200

    Add files via upload

---
+++ LinuxChallenge.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <
+
+using namespace std;
+
+int v[10];
+
+bool valid(int k)
+{
+
+}
+
+int backtr()
+{
+    int k;
+    bool cont;
+    k = 0;
+    v[k] = -1;
+    while(k < 9)
+    {
+        cont = false;
+        while(v[k] < 46 && !cont)
+        {
+            v[k]++;
+            if(valid(k))
+                cont = true;
+        }
+        if(cont == false)
+            k--;
+        else if(k == 8)
+            return 1;
+        else
+        {
+            k++;
+            v[k] = -1;
+        }
+    }
+}
+
+int main()
+{
+    return 0;
+}
