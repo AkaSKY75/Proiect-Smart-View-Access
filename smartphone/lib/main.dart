e2abcf43116099eb553aab3252c17922f0f6ec1b
Edward <edwardhalapi@hsoftdev.org>
2021-04-08 14:49:32 +0300

    login screen done

--- smartphone/lib/main.dart
+++ smartphone/lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:smartphone/screens/Login_screen.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'locator.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:async';
 
-void main() async {
-  await Firebase.initializeApp();
+void main() {
+  //await Firebase.initializeApp();
   setupLocator();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,6 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: Login(),
       navigatorKey: StackedService.navigatorKey,
       debugShowCheckedModeBanner: false,
     );
--- smartphone/lib/screens/Login_screen.dart
+++ smartphone/lib/screens/Login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:smartphone/screens/Profile_screen.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -8,8 +10,78 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Spacer(),
+          Container(
+            width: 400,
+            decoration: BoxDecoration(color: Colors.grey[100]),
+            child: TextFormField(
+              style: GoogleFonts.montserrat(
+                fontSize: 15.5,
+                color: Colors.black,
+              ),
+              decoration: InputDecoration(
+                hintText: "Introduceti mailul firmei",
+                contentPadding: EdgeInsets.only(
+                  left: 15,
+                ),
+                hintStyle: GoogleFonts.montserrat(
+                  fontSize: 15.5,
+                  color: Colors.grey[400],
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 50,
+          ),
+          Container(
+            width: 400,
+            decoration: BoxDecoration(color: Colors.grey[100]),
+            child: TextFormField(
+              style: GoogleFonts.montserrat(
+                fontSize: 15.5,
+                color: Colors.black,
+              ),
+              decoration: InputDecoration(
+                hintText: "Introduceti parola",
+                contentPadding: EdgeInsets.only(
+                  left: 15,
+                ),
+                hintStyle: GoogleFonts.montserrat(
+                  fontSize: 15.5,
+                  color: Colors.grey[400],
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+          Spacer(),
+          Container(
+              width: 200,
+              decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  borderRadius: BorderRadius.circular(10)),
+              child: MaterialButton(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  onPressed: () {})),
+          Spacer(),
+        ],
+      ),
     );
   }
 }
